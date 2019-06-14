3ed3009229ef24b738de2fe321581cc7006ad67e
Brad Schlintz <brschlin@microsoft.com>
2019-06-14 10:39:22 -0500

    Added standard disclaimer and removed extra ps help attribute

--- Remove-UnifiedGroupViaGraph.ps1
+++ Remove-UnifiedGroupViaGraph.ps1
@@ -1,9 +1,17 @@
-﻿<#
-  .SYNOPSIS
-  Multi-use script to report on all Office 365 Groups in your tenant. Once you've looked over the output CSV report, if there are groups you wish to delete, you can feed the
-  CSV report back into this script in Remove mode to delete the Office 365 groups by their id. Be sure the CSV that is fed back in contains ONLY the groups you wish to DELETE.
+﻿<# 
+ Microsoft provides programming examples for illustration only, without warranty either expressed or
+ implied, including, but not limited to, the implied warranties of merchantability and/or fitness 
+ for a particular purpose. We grant You a nonexclusive, royalty-free right to use and modify the 
+ Sample Code and to reproduce and distribute the object code form of the Sample Code, provided that
+ You agree: (i) to not use Our name, logo, or trademarks to market Your software product in which the
+ Sample Code is embedded; (ii) to include a valid copyright notice on Your software product in which
+ the Sample Code is embedded; and (iii) to indemnify, hold harmless, and defend Us and Our suppliers
+ from and against any claims or lawsuits, including attorneys' fees, that arise or result from the
+ use or distribution of the Sample Code.
+#>
 
- .DESCRIPTION
+<#
+  .SYNOPSIS
   Multi-use script to report on all Office 365 Groups in your tenant. Once you've looked over the output CSV report, if there are groups you wish to delete, you can feed the
   CSV report back into this script in Remove mode to delete the Office 365 groups by their id. Be sure the CSV that is fed back in contains ONLY the groups you wish to DELETE.
 
