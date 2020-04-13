d42c8003d486bf2227762e9fa9c21e6e33d05e4f
Brad Schlintz <brschlin@microsoft.com>
2020-04-13 16:07:52 -0500

    Updated retry logging

--- Copy-GroupMailboxAttachmentsToSPOLibrary.ps1
+++ Copy-GroupMailboxAttachmentsToSPOLibrary.ps1
@@ -72,9 +72,7 @@ function Invoke-RestMethodWithRetry
         [ValidateNotNullOrEmpty()]
         [hashtable] $Params,
         [int] $RetryCount = 5,
-        [int] $TimeoutInSecs = 10,
-        [string] $SuccessMessage = "Command executed successfuly!",
-        [string] $FailureMessage = "Failed to execute the command"
+        [int] $TimeoutInSecs = 10
     )
         
     process {
@@ -90,17 +88,17 @@ function Invoke-RestMethodWithRetry
 
                 # flow control will execute the next line only if the command in the scriptblock executed without any errors
                 # if an error is thrown, flow control will go to the 'catch' block
-                Write-Verbose "$SuccessMessage `n"
                 $Flag = $false
             }
             catch {
                 if ($Attempt -gt $RetryCount) {
-                    Write-Verbose "$FailureMessage! Total retry attempts: $RetryCount"
-                    Write-Verbose "[Error Message] $($_.exception.message) `n"
+                    Write-Host "Failed to execute Invoke-RestMethod. Total retry attempts: $RetryCount"
+                    Write-Host "  [Error] $($_.exception.message) `n"
                     $Flag = $false
                 }
                 else {
-                    Write-Verbose "[$Attempt/$RetryCount] $FailureMessage. Retrying in $TimeoutInSecs seconds..."
+                    Write-Host "[$Attempt/$RetryCount] Failed to execute Invoke-RestMethod. Retrying in $TimeoutInSecs seconds..."
+                    Write-Host "  [Error] $($_.exception.message) `n"
                     Start-Sleep -Seconds $TimeoutInSecs
                     $Attempt = $Attempt + 1
                 }
